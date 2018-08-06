907c95e4cfb343db9e3c9b48604cf8254b8727bd
Adam Johnson <adamwjo@gmail.com>
2018-08-06 12:31:06 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,11 +20,9 @@ def bonus
    }
   }
 
-  x = epic_tragedy[:montague[:hero[:status["dead"]]]]
-  y = epic_tragedy[:capulet[:heroine[:status["dead"]]]]
+ epic_tragedy[:montague[:hero[:status["dead"]]]]
+ epic_tragedy[:capulet[:heroine[:status["dead"]]]]
 
-  x
-  y
   
 
 
